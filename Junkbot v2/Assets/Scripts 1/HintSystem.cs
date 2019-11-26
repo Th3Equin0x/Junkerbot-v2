1968d0f432ec2a2c776b76f1095838cd486c7992
Th3Equin0x <sgruver22@gmail.com>
2019-11-25 23:29:24 -0600

    Moved necessary assets from old project

---
+++ Junkbot v2/Assets/Scripts 1/HintSystem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintSystem : MonoBehaviour
+{
+    public AudioClip hintsystemsfx;
+    public GameObject HintsystemCanvas;
+    public GameObject Imagehelper;
+
+    private bool helpActive = false;
+
+    void Update()
+    {
+        HintsystemCanvas.SetActive(true);
+        /*if (Input.GetKeyDown(KeyCode.Q))
+        {
+            
+            if (HintsystemCanvas == true)
+            {
+                print("Q was pressed");
+                AudioManager.Instance.PlaySFX(hintsystemsfx, 1.0f);
+                Imagehelper.SetActive(true);
+                
+            }
+          if(Imagehelper ==true)
+            {
+                if (Input.GetKeyDown(KeyCode.Q))
+                {
+                    Imagehelper.SetActive(false);
+                }
+                    
+            }
+       
+            
+        }*/
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (helpActive == true)
+            {
+                helpActive = false;
+                Imagehelper.SetActive(false);
+            }
+            else
+            {
+                helpActive = true;
+                Imagehelper.SetActive(true);
+                AudioManager.Instance.PlaySFX(hintsystemsfx, 1.0f);
+            }
+        }
+    }
+}
